0402065c63f0a42c8958ee195ac7cda7a73ba241
VipCoder8 <114797944+VipCoder8@users.noreply.github.com>
2025-03-06 17:46:15 +0300

    Initial commit.

--- src/main/java/net/vipryx/mixin/AbstractHorseEntityMixin.java
+++ src/main/java/net/vipryx/mixin/AbstractHorseEntityMixin.java
@@ -1,13 +1,11 @@
 package net.vipryx.mixin;
 
 import net.minecraft.entity.passive.AbstractHorseEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.vipryx.ArmorCheck;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractHorseEntity.class)
@@ -17,9 +15,5 @@ public class AbstractHorseEntityMixin {
         if(ArmorCheck.isHorseArmor(item)) {
             cir.setReturnValue(true);
         }
-    }
-    @Inject(at = @At("RETURN"), method = "equipHorseArmor")
-    public void equip(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
-
     }
 }
